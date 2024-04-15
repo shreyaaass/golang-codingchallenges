980fcc4d8834a7eef0dd9a7b69483272d8920f24
shreyaaass <shreyas.10.singh@gmail.com>
2024-04-15 14:41:31 +0530

    Either c or m

--- wc tool/main.go
+++ wc tool/main.go
@@ -10,11 +10,11 @@ import (
 
 func main() {
 	enableBytes := flag.Bool("c", false, "outputs the number of bytes in a file")
-	enableLines := flag.Bool("l", false, "outputs the number of bytes in a file")
-	enableWords := flag.Bool("w", false, "outputs the number of bytes in a file")
+	enableLines := flag.Bool("l", false, "outputs the number of lines in a file")
+	enableWords := flag.Bool("w", false, "outputs the number of words in a file")
 	enableChars := flag.Bool("m", false, "outputs the number of characters in a file")
 	file_path := flag.String("f", "test.txt", "path to the file")
-
+	var bytesOrNot int=2
 	flag.Parse()
 	data, err := os.ReadFile(*file_path)
 	if err != nil {
@@ -27,16 +27,21 @@ func main() {
 		*enableWords=true
 	}
 	if *enableBytes{
-		fmt.Print(len(data),"	")
+		bytesOrNot=0
+	}
+	if *enableChars{
+		bytesOrNot=1
+	}
+	if bytesOrNot==0{
+		fmt.Print(len(data), "	")
+	}else if bytesOrNot==1{
+		fmt.Print(utf8.RuneCountInString(string(data)),"	")
 	}
 	if *enableLines{
 		fmt.Print(strings.Count(string(data), string("\n")),"	")
 	}
 	if *enableWords{
 		fmt.Print(strings.Count(string(data),string(" ")), "	")
-	}
-	if *enableChars{
-		fmt.Print(utf8.RuneCountInString(string(data)),"	")
 	}
 	fmt.Println(*file_path)
 
